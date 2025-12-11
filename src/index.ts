a52e5decc3f46435b60506d230f9b7e9700eed9a
harrhx <harshit.x.singh@gmail.com>
2025-12-11 18:18:23 +0530

    feat: add additional domains to CORS allowed origins

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,9 @@ const allowedOrigins = [
   "https://lively-sky-010d8a300.3.azurestaticapps.net",
   "https://jolly-dune-0e680e100.3.azurestaticapps.net",
   "https://ekeca.vercel.app",
-  "https://admin-mock-dashboard.vercel.app"
+  "https://admin-mock-dashboard.vercel.app",
+  "https://oneeka.vercel.app",
+  "https://admin-mock-dashboard-5sb7.vercel.app"
 ];
 
 app.use(cors({
